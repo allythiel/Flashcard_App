a8394c54f056064b222888b03753f05d80a90df5
allythiel <allythiel@gmail.com>
2021-06-30 19:11:37 -0500

    Add CardDeck Get and Post Requests

--- models/collection.js
+++ models/collection.js
@@ -1,23 +1,37 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const collectionSchema = new mongoose.Schema({
+const FlashcardSchema = new mongoose.Schema({
     term: { type: String, required: true, minlength: 2, maxlength: 100 },
     description: { type: String, required: true, minlength: 2, maxlength: 255 },
     dateModified: { type: Date, default: Date.now },
 });
 
-const Collection = mongoose.model('Collection', collectionSchema);
+const Flashcard = mongoose.model('Flashcard', FlashcardSchema);
 
-function validateCollection(collection) {
+function validateFlashcard(Flashcard) {
     const schema = Joi.object({
         term: Joi.string().min(2).max(50).required(),
         description: Joi.string().required(),
     });
-    return schema.validate(collection);
+    return schema.validate(Flashcard);
 }
 
+const userSchema = new mongoose.Schema({
+    cardDeck: { type: [FlashcardSchema], default: [] },
+});
 
-exports.Collection = Collection;
-exports.validate = validateCollection;
-exports.collectionSchema = collectionSchema;
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = Joi.object({
+    });
+    return schema.validate(user);
+}
+
+exports.Flashcard = Flashcard;
+exports.User = User;
+exports.validate = validateUser;
+exports.validate = validateFlashcard;
+exports.FlashcardSchema = FlashcardSchema;
+exports.userSchema = userSchema;
--- routes/collections.js
+++ routes/collections.js
@@ -1,11 +1,53 @@
-const { Collection, validate } = require('../models/collection');
+const { Flashcard, validate } = require('../models/collection');
+const { User } = require('../models/collection');
 const express = require('express');
 const router = express.Router();
 
+router.get('/:userId/cardDeck/', async (req, res) => {
+    try {
+        const user = await User.find();
+        return res.status(200).send(user);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get('/:userId/cardDeck/:flashcardId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if(!user)
+            return res.status(400).send(`The user with id: "${req.params.id}" does not exist.`);
+
+        return res.send(user);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+router.post('/:userId/cardDeck/:flashcardId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userID);
+        if (!user) return res.status(400).send(`The user with id "${req.params.userID}" does not exist`);
+
+        const flashcard = await Flashcard.findById(req.params.flashcardId);
+        if (!flashcard) return res.status(400).send(`The flashcard with the id "${req.params.productId}" does not exist`);
+
+        user.CardDeck.push(flashcard);
+
+        await user.save();
+        return res.send(user.CardDeck);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
 router.get('/', async (req, res) => {
     try {
-        const collections = await Collection.find();
-        return res.status(200).send(collections);
+        const flashcards = await Flashcard.find();
+        return res.status(200).send(flashcards);
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
@@ -13,12 +55,12 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        const collection = await Collection.findById(req.params.id);
+        const flashcard = await Flashcard.findById(req.params.id);
 
-        if(!collection)
-            return res.status(400).send(`The collection with id: "${req.params.id}" does not exist.`);
+        if(!flashcard)
+            return res.status(400).send(`The flashcard with id: "${req.params.id}" does not exist.`);
 
-        return res.send(collection);
+        return res.send(flashcard);
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
@@ -30,16 +72,55 @@ router.post('/', async (req, res) => {
         if(error)
             return res.status(400).send(error);
 
-        const collection = new Collection({
+        const flashcard = new Flashcard({
             term: req.body.term,
             description: req.body.description,
         });
 
-        await collection.save();
+        await flashcard.save();
 
-        return res.send(collection);
+        return res.send(flashcard);
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+    const flashcard = await Flashcard.findByIdAndUpdate(
+        req.params.id,
+        {
+            term: req.body.term,
+            description: req.body.description,
+        },
+        { new: true }
+    );
+
+    if(!flashcard)
+        return res.status(400).send(`The flashcard with id: "${req.params.id}" does not exist.`);
+
+    await flashcard.save();
+
+    return res.send(flashcard);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const flashcard = await Flashcard.findByIdAndRemove(req.params.id);
+
+        if (!flashcard)
+        return res.status(400).send(`The flashcard with id: "${req.params.id}" does not exist.`);
+
+        return res.send(flashcard);
+    } catch (ex) {
+        return res.status(500).send(`Internal Servor Error: ${ex}`);
     }
 });
 
