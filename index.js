c21f609aa63d20c5e28bb5338a4674ca207e9078
allythiel <allythiel@gmail.com>
2021-07-07 16:56:15 -0500

    Final Commit

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 const collections = require('./routes/collections');
+const cors = require('cors')
 
 
 connectDB();
 
 app.use(express.json());
+app.use(cors())
 app.use('/api/collections', collections);
 
 const port = process.env.PORT || 5000;
